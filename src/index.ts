c21a85c6dcac8de0bda0d671eaeba6425e075a85
Andres <andres.z.jimenez15@gmail.com>
2022-07-16 12:48:28 -0500

    Upload host

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import app from "./app";
 import { AppDataSource } from "./db";
-import { PORT } from "./config";
 
 // Inicializacion de la API
 const main = async () => {
   try {
     await AppDataSource.initialize();
     console.log('Database initialized')
-    app.listen(PORT);
+    app.listen();
     // console.log(`Server on http://localhost:${PORT}/graphql`);
   } catch (error) {
     console.error(error);
